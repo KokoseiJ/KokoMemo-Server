4b3cd0eabf88854beda9950b5e6eb3d3ef85a283
KokoseiJ <kokoseij@gmail.com>
2020-11-05 03:45:56 +0900

    Changed to retrieve SECRET_KEY from hard coded value to environment variable

--- config.py
+++ config.py
@@ -9,4 +9,4 @@ PATH = os.path.dirname(__file__)
 SQLALCHEMY_DATABASE_URI = f"sqlite:///{PATH}/data.db"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SECRET_KEY = b"test-secret-key"
+SECRET_KEY = os.environ.get('SECRET_KEY')
