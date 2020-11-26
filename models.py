fee05bda374204a8a937e672156c40eb67d151d2
KokoseiJ <kokoseij@gmail.com>
2020-11-26 10:51:05 +0900

    Modified models to contain contents, added new models

--- models.py
+++ models.py
@@ -28,14 +28,17 @@ class User(db.Model):
         db.String(50),
         nullable=False
     )
-
+    recent_token_issued_time = db.Column(
+        db.BigInteger,
+        nullable=True
+    )
     def __repr__(self):
         return f'<User {self.username}>'
 
 
 class Note(db.Model):
     id = db.Column(
-        db.String(11),
+        db.String(12),
         unique=True,
         nullable=False,
         primary_key=True
@@ -48,6 +51,14 @@ class Note(db.Model):
         db.String(50),
         nullable=False
     )
+    content =  db.Column(
+        db.String(),
+        nullable=False
+    )
+    version = db.Column(
+        db.String(8),
+        nullable=False
+    )
     created_time = db.Column(
         db.BigInteger,
         nullable=False
@@ -56,12 +67,20 @@ class Note(db.Model):
         db.BigInteger,
         nullable=True
     )
-    version = db.Column(
-        db.String(8),
-        nullable=False
+    parent_id = db.Column(
+        db.String(12),
+        db.ForeignKey("Folder.id"),
+        nullable=True
+    )
+    parent = db.relationship(
+        'Folder',
+        backref=db.backref(
+            'contents',
+            lazy=True
+        )
     )
     user_id = db.Column(
-        db.String(50),
+        db.Integer,
         db.ForeignKey("user.id"),
         nullable=False
     )
@@ -75,3 +94,97 @@ class Note(db.Model):
 
     def __repr__(self):
         return f'<Note {self.id}:{self.user.username}>'
+
+class File(db.Model):
+    id = db.Column(
+        db.String(12),
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    desc = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    content =  db.Column(
+        db.Bytes,
+        nullable=False
+    )
+    version = db.Column(
+        db.String(8),
+        nullable=False
+    )
+    created_time = db.Column(
+        db.BigInteger,
+        nullable=False
+    )
+    edited_time = db.Column(
+        db.BigInteger,
+        nullable=True
+    )
+    parent_id = db.Column(
+        db.String(12),
+        db.ForeignKey("Folder.id"),
+        nullable=True
+    )
+    parent = db.relationship(
+        'Folder',
+        backref=db.backref(
+            'contents',
+            lazy=True
+        )
+    )
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+    user = db.relationship(
+        'User',
+        backref=db.backref(
+            'files',
+            lazy=True
+        )
+    )
+
+    def __repr__(self):
+        return f'<File {self.id}:{self.user.username}>'
+
+class Folder(db.Model):
+    id = db.Column(
+        db.String(12),
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    title = db.Column(
+        db.String(50),
+        nullable=False
+    )
+    created_time = db.Column(
+        db.BigInteger,
+        nullable=False
+    )
+    edited_time = db.Column(
+        db.BigInteger,
+        nullable=True
+    )
+    user_id = db.Column(
+        db.String(50),
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+    user = db.relationship(
+        'User',
+        backref=db.backref(
+            'folders',
+            lazy=True
+        )
+    )
+
+    def __repr__(self):
+        return f'<Folder {self.id}:{self.user.username}>'
